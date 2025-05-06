496a87bab802045452e0a8f6ac2c4f92e66cb296
Damilare Oyinloye <oyinloyedamilare12@gmail.com>
2025-05-06 23:43:34 +0100

    JavaScript is interesting...

---
+++ index.js
@@ -0,0 +1,94 @@
+
+
+const toast = (text, background, color, position = 'right') => {
+    Toastify({
+        text,
+        duration: 3000,
+        // destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position, // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background,
+            color,
+        },
+        onClick: function () { } // Callback after click
+    }).showToast();
+}
+
+
+
+
+
+let allUsers = []
+
+if (localStorage.users) {
+    let retrieved = JSON.parse(localStorage.getItem('users'))
+    console.log(retrieved);
+    allUsers = retrieved
+} else {
+    allUsers = []
+}
+
+const signUp = () => {
+
+
+
+    if (firstName.value === '' || lastName.value === '' || email.value === '' || password.value === '') {
+        toast('Fill in the inputs', '#f00', '#fff')
+        sub.innerHTML = '...loading'
+        setTimeout(() => {
+            sub.innerHTML = 'Submit'
+        }, 2000)
+
+    } else {
+
+        sub.innerHTML = '...loading'
+        // console.log(allUsers);
+
+
+        const fName = document.getElementById('firstName').value;
+        const lName = document.getElementById('lastName').value;
+        const mail = document.getElementById('email').value;
+        const pass = document.getElementById('password').value;
+
+        const userObj = { fName, lName, mail, pass }
+
+        let found = allUsers.find(eachUser => eachUser.mail = mail)
+        console.log(found);
+        if (found = undefined) {
+            allUsers.push(userObj)
+            toast('Sign up successful', '#006400', '#fff')
+            console.log(allUsers);
+
+
+            document.getElementById('firstName').value = ''
+            document.getElementById('lastName').value = ''
+            document.getElementById('email').value = ''
+            document.getElementById('password').value = ''
+
+            localStorage.users = JSON.stringify(allUsers)
+                setTimeout(() => {
+                    window.location.href = 'signin.html'
+            }, 2000)
+            
+        } else {
+            toast('Account already exists', '#00f', '#fff')
+            setTimeout(() => {
+                window.location.href = 'signin.html'
+        }, 2000)
+
+        }
+
+
+        // allUsers.push(userObj)
+        // toast('Sign up successful', '#006400', '#fff')
+        // console.log(allUsers)
+
+    }
+
+    
+}
+
